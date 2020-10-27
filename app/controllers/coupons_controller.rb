6d397d1304fcfca87767b25e7cef1e0d9e149b1c
zachary mines <zmines@wesleyan.edu>
2020-10-27 23:21:07 +0000

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -10,5 +10,10 @@ class CouponsController < ApplicationController
   end 
   
   def create 
+    a = Coupon.new 
+    a.coupon_code = params[:coupon_code]
+    a.store = params[:store]
+    a.save 
+    redirect_to coupon_path
   end 
 end 
